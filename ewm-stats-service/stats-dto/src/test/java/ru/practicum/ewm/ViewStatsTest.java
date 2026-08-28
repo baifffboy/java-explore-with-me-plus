e67f3c0e9aa67dc7f8e0c25001eaec9a3bdfb539
baifffboy <ilya.yakimov.43@gmail.com>
2026-08-28 10:50:31 +0300

    правка кодстайла

--- ewm-stats-service/stats-dto/src/test/java/ru/practicum/ewm/ViewStatsTest.java
+++ ewm-stats-service/stats-dto/src/test/java/ru/practicum/ewm/ViewStatsTest.java
@@ -39,13 +39,11 @@ class ViewStatsTest {
 
     @Test
     void shouldDeserializeFromJson() throws Exception {
-        String json = """
-                {
-                    "app": "ewm-main-service",
-                    "uri": "/events/1",
-                    "hits": 10
-                }
-                """;
+        String json = "{" +
+                "    \"app\": \"ewm-main-service\"," +
+                "    \"uri\": \"/events/1\"," +
+                "    \"hits\": 10" +
+                "}";
 
         ViewStats dto = objectMapper.readValue(json, ViewStats.class);
 
